2b2751e2c736f1cbc569a08d040fbbc307d9f24d
Terogene Claudio <cterogene@gmail.com>
2022-07-02 21:39:10 -0500

    setting up the new menu and starting to add and link the page to the menu.

--- app/src/main/java/com/example/paniermobil/activities/MainActivity.java
+++ app/src/main/java/com/example/paniermobil/activities/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main,menu);
         return true;
     }
 
