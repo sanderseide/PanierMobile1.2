7263450a4e54272163eb4a3a24dd7590d3667f42
Terogene Claudio <cterogene@gmail.com>
2022-07-03 18:19:08 -0500

    added the updates and completed the designs.

---
+++ app/src/main/java/com/example/paniermobil/activities/new_product.java
@@ -0,0 +1,15 @@
+package com.example.paniermobil.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.paniermobil.R;
+
+public class new_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_product);
+    }
+}
