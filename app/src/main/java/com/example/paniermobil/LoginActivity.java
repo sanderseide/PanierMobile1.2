7d7aa1a7d04dfa9e447f6ac799eabf049627216d
Terogene Claudio <cterogene@gmail.com>
2022-07-02 20:48:17 -0500

    Updated some background code- managed to make the app work better with clean code.

--- app/src/main/java/com/example/paniermobil/LoginActivity.java
+++ app/src/main/java/com/example/paniermobil/LoginActivity.java
@@ -1,4 +1,5 @@
 package com.example.paniermobil;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,6 +11,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+//import com.example.paniermobil.activities.MainActivity;
+import com.example.paniermobil.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -48,6 +54,34 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser() {
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+        if (TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Entre votre adresse email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this, "Entre votre modepass", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(userPassword.length() < 6){
+            Toast.makeText(this, "modepass trop court, minimum 6 lettre!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        auth.signInWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Error: "+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
--- app/src/main/java/com/example/paniermobil/MainActivity.java
+++ app/src/main/java/com/example/paniermobil/MainActivity.java
@@ -28,25 +28,25 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        //setSupportActionBar(binding.appBarMain.toolbar);
+//        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+       // DrawerLayout drawer = binding.drawerLayout;
+        //NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
+//        mAppBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+//                .setOpenableLayout(drawer)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+       // NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
